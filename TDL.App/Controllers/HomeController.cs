4f09684580830ae0cdd7fca5d1d56326dbd05647
rajeshnaidu4065 <rajeshnaidu.4065@gmail.com>
2022-02-04 02:44:55 +0000

    app UI changes

--- TDL.App/Controllers/HomeController.cs
+++ TDL.App/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using TDL.App.Models;
+using System;
+using System.Threading.Tasks;
+using TDL.App.Services;
+using TDL.Common;
 
 namespace TDL.App.Controllers
 {
@@ -13,15 +11,69 @@ namespace TDL.App.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IPatientAppService _iPatientAppService;
+
+
+        public HomeController(ILogger<HomeController> logger, IPatientAppService iPatientAppService)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._iPatientAppService = iPatientAppService;
         }
 
-        public IActionResult Index()
+        /// <summary>The index/list of insert sources.</summary>
+        /// <returns>The index view.</returns>
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                var result = await this._iPatientAppService.GetPatientDetails();
+                if (TempData["success"] != null)
+                {
+                    result.Status = true;
+                    result.PatientSubmitDto.LastUpdateDateTime = Convert.ToDateTime(TempData["success"]);
+                    TempData["success"] = null;
+                }
+
+                if (TempData["fail"] != null)
+                {
+                    result.Status = false;
+                    result.PatientSubmitDto.LastUpdateDateTime = Convert.ToDateTime(TempData["fail"]);
+                    TempData["fail"] = null;
+                }
+                return this.View(result);
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError(ex, $"Exception: {nameof(HomeController)}/{nameof(this.Index)}: {ex.Message}");
+                return this.RedirectToAction("Error", "Home");
+            }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(PatientSubmitDto patientInputDto)
+        {
+            try
+            {
+                var result = await this._iPatientAppService.UpdatePatientDetails(patientInputDto);
+                if (result.Status)
+                {
+                    TempData["success"] = result.LastUpdateDateTime;
+                }
+                else
+                {
+                    TempData["fail"] = DateTime.MinValue;
+                }
+
+                return this.RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError(ex, $"Exception: {nameof(HomeController)}/{nameof(this.Index)}: {ex.Message}");
+                return this.RedirectToAction("Error", "Home");
+            }
+        }
+
 
         public IActionResult Privacy()
         {
@@ -31,7 +83,7 @@ namespace TDL.App.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
--- TDL.App/Startup.cs
+++ TDL.App/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TDL.App.Data;
+using TDL.App.Services;
 
 namespace TDL.App
 {
@@ -24,6 +26,8 @@ namespace TDL.App
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IPatientAppService, PatientAppService>();
+            services.AddHttpClient<IPatientDataRepository, PatientDataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
